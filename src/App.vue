809ef6c0dad2b8788091dfe85292ad62e3d10b34
Claudio Jose <claudio.jose.sousaa@gmail.com>
2024-04-18 19:17:06 -0300

    Primeira commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+  import {ref, reactive} from 'vue'
+  import GitHub from './components/GitHub.vue';
+
+
+</script>
+
+<template>
+  <GitHub />
+
+</template>
+
+<style scoped>
+  
+</style>
